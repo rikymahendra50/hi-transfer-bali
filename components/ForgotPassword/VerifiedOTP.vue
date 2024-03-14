712690322c13767ba6805f954bde262e0d49ce7f
silvester <silvesterlhwali123@gmail.com>
2024-03-14 15:53:03 +0800

    fix

--- components/ForgotPassword/VerifiedOTP.vue
+++ components/ForgotPassword/VerifiedOTP.vue
@@ -43,61 +43,71 @@ onMounted(async () => {
     :validation-schema="otpSchema"
     v-slot="{ errors }"
   >
-    <div class="grid grid-cols-1 w-[450px] text-left gap-4 p-4 rounded-md shadow">
+    <div class="grid grid-cols-1 w-[450px] text-left gap-2 p-4 rounded-md shadow">
       <Alert
         v-model="message"
         :type="alertType"
       />
-      <FormGroup
-        label="OTP"
-        name="opt"
-      >
-        <div class="hidden">
-          <VeeField
-            name="otp"
+      <div class="flex  justify-center ">
+        <FormGroup
+          label="OTP"
+          name="opt"
+        >
+          <div class="hidden">
+            <VeeField
+              name="otp"
+              v-model="$credentialForgotPassword.pin"
+            />
+          </div>
+          <FormInputOTP
             v-model="$credentialForgotPassword.pin"
+            :is-error="!!errors?.otp"
           />
-        </div>
-        <FormInputOTP
-          v-model="$credentialForgotPassword.pin"
-          :is-error="!!errors?.otp"
-        />
-        <TransitionLeftToRight>
-          <VeeErrorMessage
-            name="otp"
-            class="form-error-message"
-          />
-        </TransitionLeftToRight>
-      </FormGroup>
-      <div v-if="$countdownHelper.showExpired">
-        <p class="text-gray-400">If you did not receive the an email <span
-            class="link"
-            @click="resentEmail"
-            role="button"
-          >click here</span>
-        </p>
+          <TransitionX>
+            <VeeErrorMessage
+              name="otp"
+              class="form-error-message"
+            />
+          </TransitionX>
+        </FormGroup>
       </div>
-
-      <div>
-        <div
-          v-if="$countdownHelper.expiredTime > 0"
-          class="text-gray-400 text-sm"
-        >
-          We have sent an OTP to your email.
-          Your OTP will expired in <span class="whitespace-nowrap">
-            {{ $countdownHelper.expiredTime }} seconds
-          </span>
-        </div>
-
-        <div
-          class="text-error  text-sm"
-          v-if="$countdownHelper.showExpired"
-        >
-          Your OTP has expired. Please request a new one
+      <div class="flex  justify-center">
+        <div v-if="$countdownHelper.showExpired">
+          <p class="text-gray-400">If you did not receive the an email <span
+              class="link"
+              @click="resentEmail"
+              role="button"
+            >click here</span>
+          </p>
         </div>
       </div>
 
-      <div>
+      <div class="flex justify-center">
+        <div>
+          <div
+            v-if="$countdownHelper.expiredTime > 0"
+            class="text-gray-400 text-sm"
+          >
+            We have sent an OTP to your email.
+            Your OTP will expired in <span class="whitespace-nowrap">
+              {{ $countdownHelper.expiredTime }} seconds
+            </span>
+          </div>
+
+          <div
+            class="text-error  text-sm"
+            v-if="$countdownHelper.showExpired"
+          >
+            Your OTP has expired. Please request a new one
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+      <div class="flex justify-center">
         <button
           type="submit"
           :disabled="loading"
