bb5abf7cf2081b51bdc02032931c4c09b0bf5ca7
silvester <silvesterlhwali123@gmail.com>
2024-03-25 09:40:07 +0800

    comming soon component

---
+++ components/ComingSoon.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="hero min-h-screen bg-gradient-to-tr from-emerald-300 to-sky-200">
+            <div class="hero-content text-center">
+                <div class="max-w-lg">
+                    <h1 class="text-3xl lg:text-7xl font-black">Coming Soon</h1>
+                    <p class="py-6 text-2xl">
+                        We glade to announce that our website is comming soon.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script
+    setup
+    lang="ts"
+>
+
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,20 @@
 <template>
-  <div class="hero min-h-screen">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
-        </p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
-    </div>
-  </div>
-  <div class="w-full  mx-auto">
-    <NuxtImg
-      src="https://images.unsplash.com/photo-1707724190331-15ac0c62f55e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      format="webp"
-    />
-    <div class="avatar">
-      <div class="h-14 w-14 rounded-full">
-        <NuxtImg
-          src="https://images.unsplash.com/photo-1654762701727-de6ccd172eb9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          width="56"
-          height="56"
-          format="webp"
-        />
-      </div>
-    </div>
-
+  <div>
+    <ComingSoon />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script
+  setup
+  lang="ts"
+>
+definePageMeta({
+  layout: "empty",
+})
+
 useHead({
-  title: "DaisyUI",
+  title: "Home",
 })
 </script>
 
-<style></style>
+<style scoped></style>
