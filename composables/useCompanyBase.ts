665ade377d1c9bbdf32e0d6c716570f0c5d7e61f
rikymahendra50 <rikymahendra50@gmail.com>
2024-10-11 14:31:04 +0800

    fix company base

--- composables/useCompanyBase.ts
+++ composables/useCompanyBase.ts
@@ -1,51 +1,52 @@
 export default function () {
+  const contact = [
+    {
+      title: "Address",
+      label:
+        "Jl. Amerta Br. Dinas Kaja , Desa/Kelurahan Busungbiu, Kec. Busungbiu, Kab. Buleleng, Provinsi Bali,Kode Pos: 81154",
+      link: "https://maps.app.goo.gl/3XkGAE8Erm2wsdLZA",
+    },
+    {
+      title: "Phone",
+      label: "0887744015600",
+      link: "tel:+6287744015600",
+    },
+    {
+      title: "Email",
+      label: "dekpericab@gmail.com",
+      link: "mailto:dekpericab@gmail.com",
+    },
+  ];
 
-    const contact = [
-        {
-            title: "Address",
-            label: "Jl. Amerta Br. Dinas Kaja , Desa/Kelurahan Busungbiu, Kec. Busungbiu, Kab. Buleleng, Provinsi Bali,Kode Pos: 81154",
-            link: "https://maps.app.goo.gl/3XkGAE8Erm2wsdLZA",
-        }, {
-            title: "Phone",
-            label: "087782660007",
-            link: "tel:+6287782660007"
-        }, {
-            title: "Email",
-            label: "dekpericab@gmail.com",
-            link: "mailto:dekpericab@gmail.com"
-        }
-    ]
+  const socialMedias = [
+    {
+      icon: "ic:baseline-facebook",
+      label: "Facebook",
+      link: "#",
+      external: true,
+    },
+    {
+      icon: "ri:twitter-x-fill",
+      label: "X/Twitter",
+      link: "#",
+      external: true,
+    },
+    {
+      icon: "ri:instagram-fill",
+      label: "Instagram",
+      link: "#",
+      external: true,
+    },
+    {
+      icon: "ri:youtube-fill",
+      label: "Youtube",
+      link: "#",
+      external: true,
+    },
+  ];
 
-
-    const socialMedias = [
-        {
-            icon: "ic:baseline-facebook",
-            label: "Facebook",
-            link: "#",
-            external: true
-        },
-        {
-            icon: "ri:twitter-x-fill",
-            label: "X/Twitter",
-            link: "#",
-            external: true,
-        },
-        {
-            icon: "ri:instagram-fill",
-            label: "Instagram",
-            link: "#",
-            external: true,
-        },
-        {
-            icon: "ri:youtube-fill",
-            label: "Youtube",
-            link: "#",
-            external: true,
-        }
-    ]
-
-    return {
-        socialMedias,
-        contact
-    }
+  return {
+    socialMedias,
+    contact,
+  };
 }
